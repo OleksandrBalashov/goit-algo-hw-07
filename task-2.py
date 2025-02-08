a71184133d1461a20721a68b0d2e39153cb0371f
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2025-02-08 09:18:32 +0100

    finish

---
+++ task-2.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert(node.right, value)
+
+    def find_max(self):
+        if self.root is None:
+            return None
+    
+        current = self.root
+        while current.right is not None:
+            current = current.right
+        return current.value
---
+++ task-3.py
@@ -0,0 +1,35 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
+        else:
+            self._insert(self.root, value)
+
+    def _insert(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self._insert(node.left, value)
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self._insert(node.right, value)
+
+    def sum_tree(self):
+        return self._sum_tree(self.root)
+
+    def _sum_tree(self, node):
+        if node is None:
+            return 0
+        return node.value + self._sum_tree(node.left) + self._sum_tree(node.right)
